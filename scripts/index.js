dfcc0177103c4313462487615e50dd82e6aba29e
Max Braun <msbraun@bsu.edu>
2021-10-28 17:35:06 -0400

    1.0.1

---
+++ scripts/index.js
@@ -0,0 +1,52 @@
+$("nav").ready(function(){
+    var windowHeight = $(window).height()
+    var navHeight = $("nav").outerHeight()
+
+    var canvasHeight = windowHeight - navHeight;
+    $("#canvas").height(canvasHeight);
+
+    typing();
+    blink();
+});
+
+var list = ["developer", "self-learner", "student", "\"gamer\"", "UI/UX enthusiast"]
+index = 0;
+
+function blink(){
+    setTimeout(function(){ 
+        $("#dash").toggle();
+        index++;
+        blink();
+    }, 500);
+}
+
+var phraseIndex = 0;
+
+async function typing() {
+    if(phraseIndex >= list.length){
+        phraseIndex = 0;
+    }
+    typePhrase(list[phraseIndex]);
+    
+}
+
+
+var letterIndex = 0;
+
+function typePhrase(phrase){
+    var deferred = $.Deferred();
+    setTimeout(function(){
+        $("#change").append(phrase.charAt(letterIndex));
+        if (letterIndex < phrase.length -1) {  
+            letterIndex++
+            typePhrase(phrase);
+        }else{
+            letterIndex = 0;
+            phraseIndex++;
+            setTimeout(function(){
+                $("#change").text("");
+                typing();
+            },1500);
+        }
+    },100);
+}
