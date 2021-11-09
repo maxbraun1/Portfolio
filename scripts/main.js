ccc1345b884e891b33980e760ed84171c979dd71
Max Braun <maxbraun175@yahoo.com>
2021-11-08 21:39:37 -0500

    Font and mobile menu update

--- scripts/main.js
+++ scripts/main.js
@@ -3,14 +3,28 @@ $(document).ready(function(){
     $("#menu-close-btn").on("click", function(){closeMenu()});
 });
 
+var menu_open = false;
+
 function openMenu(){
+    menu_open = true;
     $("#mobile-nav-background").animate({height: "100%"}, function(){
         $("#nav-links").show(400);
     });
+    menuBackgroundScroll();
 }
 
 function closeMenu(){
+    menu_open = false;
     $("#nav-links").hide(400, function(){
         $("#mobile-nav-background").animate({height: "0px"});
     });
+}
+
+function menuBackgroundScroll(){
+    if(menu_open == true){
+        $("#mobile-nav-background").css("background-position-x",0);
+        $("#mobile-nav-background").css("background-position-y",0);
+        $("#mobile-nav-background").animate({'background-position-x': 50,
+        'background-position-y': 50},500, "linear" ,function(){menuBackgroundScroll()});
+    }
 }
