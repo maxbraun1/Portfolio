2d86c4f18fbba87e1111eaae069ce7f7393cf1b0
Max Braun <maxbraun175@yahoo.com>
2023-01-29 22:26:49 -0500

    minor updates/fixes

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,30 @@
 $(document).ready(function(){
     AOS.init();
 
-    $("#primary-intro-button").click(function() {
+    $("#secondary-intro-button").click(function() {
         $('html, body').animate({
-            scrollTop: $("#about").offset().top
-        }, 1000);
+            scrollTop: $("#projects").offset().top
+        }, 1500);
+    });
+
+    $(document).mousemove(function(e) {
+        window.x = e.pageX;
+        window.y = e.pageY;
+        midpagex = $(document).width()/2;
+        midpagey = $(window).height()/2;
+
+        offsetX = Math.round(window.x - midpagex) / 15;
+        offsetY = Math.round(window.y - midpagey) / 15;
+        if(offsetX >= 0){
+            $("#blob").css({ marginRight : Math.abs(offsetX) + "px" });
+        }else{
+            $("#blob").css({ marginLeft : Math.abs(offsetX) + "px" });
+        }
+
+        if(offsetY <= 0){
+            $("#blob").css({ marginTop : Math.abs(offsetY) + "px" });
+        }else{
+            $("#blob").css({ marginBottom : Math.abs(offsetY) + "px" });
+        }
     });
 });
