ecb8ed3482870a9d2348dbe86248a005e0dea0ac
Max Braun <maxbraun175@yahoo.com>
2023-01-29 21:40:16 -0500

    refresh

--- scripts/index.js
+++ scripts/index.js
@@ -1,67 +1,9 @@
 $(document).ready(function(){
-    typing();
-    blink();
-    centerOrbit();
     AOS.init();
+
+    $("#primary-intro-button").click(function() {
+        $('html, body').animate({
+            scrollTop: $("#about").offset().top
+        }, 1000);
+    });
 });
-
-$(window).resize(function(){
-    centerOrbit();
-});
-
-var list = ["developer", "self learner", "student", "UI Lover"]
-index = 0;
-
-function blink(){
-    setTimeout(function(){ 
-        $("#dash").toggle();
-        index++;
-        blink();
-    }, 500);
-}
-
-var phraseIndex = 0;
-
-async function typing() {
-    if(phraseIndex >= list.length){
-        phraseIndex = 0;
-    }
-    typePhrase(list[phraseIndex]); 
-}
-
-
-var letterIndex = 0;
-
-function typePhrase(phrase){
-    var deferred = $.Deferred();
-    setTimeout(function(){
-        $("#change").append(phrase.charAt(letterIndex));
-        if (letterIndex < phrase.length -1) {  
-            letterIndex++
-            typePhrase(phrase);
-        }else{
-            letterIndex = 0;
-            phraseIndex++;
-            setTimeout(function(){
-                $("#change").text("");
-                typing();
-            },1500);
-        }
-    },100);
-}
-
-function centerOrbit(){
-    var width = $(window).width();
-    var height = $(window).height();
-    var half = $("#orbit").width()/2;
-
-    $("#orbit").css("bottom",-half);
-
-    if($(window).width() < 1000){
-        var middle = width/2 - half;
-        $("#orbit").css("left",middle);
-    }else{
-        $("#orbit").css("right",-half);
-        $("#orbit").css("left","auto");
-    }
-}
